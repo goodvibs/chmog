77844f7512e144cfd2294850bda31a97bdb44277
Vibhav Singamshetty <vibhav.singamshetty@vibhav.singamshetty-Mac>
2025-07-07 19:00:16 -0700

    feat: Add API for attacks module

--- src/attacks/mod.zig
+++ src/attacks/mod.zig
@@ -2,3 +2,12 @@ pub const manual = @import("manual.zig");
 pub const magic = @import("magic.zig");
 pub const precomputed = @import("precomputed.zig");
 const generated = @import("generated/mod.zig");
+
+pub const multiPawnPushes = manual.multiPawnPushes;
+pub const multiPawnAttacks = manual.multiPawnAttacks;
+pub const multiKnightAttacks = manual.multiKnightAttacks;
+pub const singleKnightAttacks = precomputed.singleKnightAttacks;
+pub const singleBishopAttacks = magic.singleBishopAttacks;
+pub const singleRookAttacks = magic.singleRookAttacks;
+pub const multiKingAttacks = manual.multiKingAttacks;
+pub const singleKingAttacks = precomputed.singleKingAttacks;
