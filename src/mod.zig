f267f79df48c74d0e35953872f7151ef9e4010e6
Vibhav Singamshetty <vibhav.singamshetty@vibhav.singamshetty-Mac>
2025-07-10 23:39:52 -0700

    feat: Add files to mod.zig

--- src/mod.zig
+++ src/mod.zig
@@ -1,6 +1,10 @@
+pub usingnamespace @import("board.zig");
+pub usingnamespace @import("castlingRights.zig");
 pub usingnamespace @import("color.zig");
 pub usingnamespace @import("file.zig");
 pub usingnamespace @import("piece.zig");
+pub usingnamespace @import("position.zig");
+pub usingnamespace @import("positionContext.zig");
 pub usingnamespace @import("rank.zig");
 pub usingnamespace @import("square.zig");
 
@@ -8,5 +12,6 @@ pub const attacks = @import("attacks/mod.zig");
 pub const masks = @import("masks.zig");
 pub const move = @import("move/mod.zig");
 pub const utils = @import("utils/mod.zig");
+pub const zobrist = @import("zobrist.zig");
 
 pub const Bitboard = u64;
