7d064666156758ff37dac5dad3cf5fdf5141112f
Vibhav Singamshetty <vibhav.singamshetty@vibhav.singamshetty-Mac>
2025-07-10 23:39:37 -0700

    feat: Start work on implementing full position representation

---
+++ src/position.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const ArrayList = @import("std").ArrayList;
+const Bitboard = @import("./mod.zig").Bitboard;
+const Board = @import("./mod.zig").Board;
+const Color = @import("./mod.zig").Color;
+const GameResult = @import("./mod.zig").GameResult;
+const PositionContext = @import("./mod.zig").PositionContext;
+
+pub const Position = struct {
+    board: Board,
+    contexts: ArrayList(PositionContext),
+    halfmove: u10,
+    gameResult: ?GameResult,
+    sideToMove: Color,
+
+    pub fn initial() Position {
+        return Position{
+            .board = Board.initial(),
+            .contexts = ArrayList(PositionContext).init(std.heap.page_allocator),
+            .halfmove = 0,
+            .gameResult = null,
+            .sideToMove = Color.White,
+        };
+    }
+
+    pub fn context(self: *const Position) *const PositionContext {
+        return &self.contexts.items[self.contexts.items.len - 1];
+    }
+
+    pub fn contextMut(self: *Position) *PositionContext {
+        return &self.contexts.items[self.contexts.items.len - 1];
+    }
+};
