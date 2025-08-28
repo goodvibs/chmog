fadb88305ba054d12db6becd7c302d1e83b07e8e
Vibhav Singamshetty <vibhav.singamshetty@geniussports.com>
2025-08-27 20:14:50 -0700

    feat: Move main to bin/cli

---
+++ bin/cli.zig
@@ -0,0 +1,19 @@
+const print = @import("std").debug.print;
+const io = @import("std").io;
+const chmog = @import("chmog");
+
+pub fn main() !void {
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    print("All your {s} are belong to us.\n", .{"codebase"});
+
+    // stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not any debugging messages.
+    const stdout_file = io.getStdOut().writer();
+    var bw = io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    try bw.flush(); // Don't forget to flush!
+}
