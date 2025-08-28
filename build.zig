5e199ff04857d1aaa7e8347153e78351156f34ca
Vibhav Singamshetty <vibhav.singamshetty@geniussports.com>
2025-08-27 20:17:16 -0700

    feat: Rename lib from chmog_lib to chmog; update

--- build.zig
+++ build.zig
@@ -23,26 +23,10 @@ pub fn build(b: *std.Build) void {
         // only contains e.g. external object files, you can make this `null`.
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/mod.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    // We will also create a module for our other entry point, 'main.zig'.
-    const exe_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // Modules can depend on one another using the `std.Build.Module.addImport` function.
-    // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
-    // file path. In this case, we set up `exe_mod` to import `lib_mod`.
-    exe_mod.addImport("chmog_lib", lib_mod);
 
     // Now, we will create a static library based on the module we created above.
     // This creates a `std.Build.Step.Compile`, which is the build step responsible
@@ -58,41 +42,6 @@ pub fn build(b: *std.Build) void {
     // running `zig build`).
     b.installArtifact(lib);
 
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
-    const exe = b.addExecutable(.{
-        .name = "chmog",
-        .root_module = exe_mod,
-    });
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
@@ -101,26 +50,19 @@ pub fn build(b: *std.Build) void {
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
 
     // Generation step
     const generate_mod = b.createModule(.{
-        .root_source_file = b.path("tools/generateMagicLookups.zig"),
+        .root_source_file = b.path("bin/generateMagicLookups.zig"),
         .target = target,
         .optimize = optimize,
     });
-    generate_mod.addImport("chmog_lib", lib_mod);
+    generate_mod.addImport("chmog", lib_mod);
 
     const generate_exe = b.addExecutable(.{
         .name = "generate-magic-lookups",
@@ -130,10 +72,10 @@ pub fn build(b: *std.Build) void {
     const generate_step = b.step("generate", "Generate magic lookup tables");
 
     const bishop_cmd = b.addRunArtifact(generate_exe);
-    bishop_cmd.addArgs(&[_][]const u8{"bishop", "src/attacks/generated/bishopMagicInfoLookup.zig"});
+    bishop_cmd.addArgs(&[_][]const u8{ "bishop", "src/attacks/generated/bishopMagicInfoLookup.zig" });
 
     const rook_cmd = b.addRunArtifact(generate_exe);
-    rook_cmd.addArgs(&[_][]const u8{"rook", "src/attacks/generated/rookMagicInfoLookup.zig"});
+    rook_cmd.addArgs(&[_][]const u8{ "rook", "src/attacks/generated/rookMagicInfoLookup.zig" });
 
     generate_step.dependOn(&bishop_cmd.step);
     generate_step.dependOn(&rook_cmd.step);
