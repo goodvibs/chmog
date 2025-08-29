02c5fee7481ca105ce59e07523341fca383e1ed9
Vibhav Singamshetty <vibhav.singamshetty@geniussports.com>
2025-08-29 03:45:24 -0700

    chore: Add deinit for gpa

--- bin/generateMagicLookups.zig
+++ bin/generateMagicLookups.zig
@@ -42,6 +42,7 @@ fn parseArgs(args: [][:0]u8) !GenerateOptions {
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
