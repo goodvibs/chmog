d648feef28090671985658ff64d765355a1674f2
Vibhav Singamshetty <vibhav.singamshetty@geniussports.com>
2025-09-06 19:19:12 -0700

    feat: Implement knight movegen logic

--- src/position.zig
+++ src/position.zig
@@ -1,10 +1,16 @@
 const std = @import("std");
 const ArrayList = @import("std").ArrayList;
+const Move = @import("./mod.zig").Move;
+const MoveFlag = @import("./mod.zig").MoveFlag;
 const Bitboard = @import("./mod.zig").Bitboard;
 const Board = @import("./mod.zig").Board;
+const Piece = @import("./mod.zig").Piece;
+const Square = @import("./mod.zig").Square;
 const Color = @import("./mod.zig").Color;
 const GameResult = @import("./mod.zig").GameResult;
 const PositionContext = @import("./mod.zig").PositionContext;
+const iterSetBits = @import("./mod.zig").utils.iterSetBits;
+const singleKnightAttacks = @import("./mod.zig").attacks.singleKnightAttacks;
 
 pub const Position = struct {
     board: Board,
@@ -29,5 +35,23 @@ pub const Position = struct {
 
     pub fn contextMut(self: *Position) *PositionContext {
         return &self.contexts.items[self.contexts.items.len - 1];
+    }
+
+    pub fn addLegalKnightMoves(self: *const Position, moves: *ArrayList(Move), allocator: std.mem.Allocator) !void {
+        const movableKnights = self.board.mask(Piece.Knight, self.sideToMove) & ~self.context().pinned;
+        const currentSidePieces = self.board.colorMask(self.sideToMove);
+
+        var sourceMasksIter = iterSetBits(movableKnights);
+        while (sourceMasksIter.next()) |sourceMask| {
+            const source = Square.fromMask(sourceMask) catch unreachable;
+            const knightAttacks = singleKnightAttacks(source);
+            const knightMoves = knightAttacks & ~currentSidePieces;
+
+            var destMasksIter = iterSetBits(knightMoves);
+            while (destMasksIter.next()) |destMask| {
+                const dest = Square.fromMask(destMask) catch unreachable;
+                try moves.append(allocator, Move.newNonPromotion(source, dest, MoveFlag.Normal));
+            }
+        }
     }
 };
