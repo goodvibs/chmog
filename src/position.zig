62048234716423d5278e8b7602816bf230f9fda3
Vibhav Singamshetty <vibhav.singamshetty@geniussports.com>
2025-09-06 19:59:51 -0700

    feat: Implement method to add sliding piece moves to move list, improvements/changes

--- src/position.zig
+++ src/position.zig
@@ -11,6 +11,9 @@ const GameResult = @import("./mod.zig").GameResult;
 const PositionContext = @import("./mod.zig").PositionContext;
 const iterSetBits = @import("./mod.zig").utils.iterSetBits;
 const singleKnightAttacks = @import("./mod.zig").attacks.singleKnightAttacks;
+const singleBishopAttacks = @import("./mod.zig").attacks.singleBishopAttacks;
+const singleRookAttacks = @import("./mod.zig").attacks.singleRookAttacks;
+const edgeToEdge = @import("./mod.zig").utils.edgeToEdge;
 
 pub const Position = struct {
     board: Board,
@@ -37,17 +40,48 @@ pub const Position = struct {
         return &self.contexts.items[self.contexts.items.len - 1];
     }
 
-    pub fn addLegalKnightMoves(self: *const Position, moves: *ArrayList(Move), allocator: std.mem.Allocator) !void {
+    pub fn addLegalKnightMoves(self: *const Position, comptime allocator: std.mem.Allocator, moves: *ArrayList(Move)) !void {
         const movableKnights = self.board.mask(Piece.Knight, self.sideToMove) & ~self.context().pinned;
         const currentSidePieces = self.board.colorMask(self.sideToMove);
 
         var sourceMasksIter = iterSetBits(movableKnights);
         while (sourceMasksIter.next()) |sourceMask| {
             const source = Square.fromMask(sourceMask) catch unreachable;
-            const knightAttacks = singleKnightAttacks(source);
-            const knightMoves = knightAttacks & ~currentSidePieces;
+            const attacks = singleKnightAttacks(source);
+            const filteredAttacks = attacks & ~currentSidePieces;
 
-            var destMasksIter = iterSetBits(knightMoves);
+            var destMasksIter = iterSetBits(filteredAttacks);
+            while (destMasksIter.next()) |destMask| {
+                const dest = Square.fromMask(destMask) catch unreachable;
+                try moves.append(allocator, Move.newNonPromotion(source, dest, MoveFlag.Normal));
+            }
+        }
+    }
+
+    pub fn addLegalSlidingPieceMoves(self: *const Position, comptime piece: Piece, comptime allocator: std.mem.Allocator, moves: *ArrayList(Move)) !void {
+        const movablePieces = self.board.mask(piece, self.sideToMove);
+        const occupiedMask = self.board.occupiedMask();
+        const currentSidePieces = self.board.colorMask(self.sideToMove);
+        const currentSideKingSquare = Square.fromMask(self.board.mask(Piece.King, self.sideToMove)) catch return error.MultipleKingsForColor;
+
+        var sourceMasksIter = iterSetBits(movablePieces);
+        while (sourceMasksIter.next()) |sourceMask| {
+            const source = Square.fromMask(sourceMask) catch unreachable;
+            const attacks = comptime if (piece == Piece.Bishop) attacks: {
+                break :attacks singleBishopAttacks(source, occupiedMask);
+            } else if (piece == Piece.Rook) attacks: {
+                break :attacks singleRookAttacks(source, occupiedMask);
+            } else if (piece == Piece.Queen) attacks: {
+                break :attacks singleBishopAttacks(source, occupiedMask) | singleRookAttacks(source, occupiedMask);
+            };
+
+            var filteredAttacks = attacks & ~currentSidePieces;
+            if (sourceMask & self.context().pinned != 0) {
+                const attacksFilter = edgeToEdge(source, currentSideKingSquare);
+                filteredAttacks &= attacksFilter;
+            }
+
+            var destMasksIter = iterSetBits(filteredAttacks);
             while (destMasksIter.next()) |destMask| {
                 const dest = Square.fromMask(destMask) catch unreachable;
                 try moves.append(allocator, Move.newNonPromotion(source, dest, MoveFlag.Normal));
