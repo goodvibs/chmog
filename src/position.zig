aeca976689a8f3fc2306bca8b7f5180f1163683a
Vibhav Singamshetty <vibhav.singamshetty@geniussports.com>
2025-09-07 04:06:25 -0700

    feat: Add method to calculate king moves

--- src/position.zig
+++ src/position.zig
@@ -10,10 +10,13 @@ const Color = @import("./mod.zig").Color;
 const GameResult = @import("./mod.zig").GameResult;
 const PositionContext = @import("./mod.zig").PositionContext;
 const iterSetBits = @import("./mod.zig").utils.iterSetBits;
+const multiPawnAttacks = @import("./mod.zig").attacks.multiPawnAttacks;
 const singleKnightAttacks = @import("./mod.zig").attacks.singleKnightAttacks;
 const singleBishopAttacks = @import("./mod.zig").attacks.singleBishopAttacks;
 const singleRookAttacks = @import("./mod.zig").attacks.singleRookAttacks;
+const singleKingAttacks = @import("./mod.zig").attacks.singleKingAttacks;
 const edgeToEdge = @import("./mod.zig").utils.edgeToEdge;
+const between = @import("./mod.zig").utils.between;
 
 pub const Position = struct {
     board: Board,
@@ -86,6 +89,47 @@ pub const Position = struct {
                 const dest = Square.fromMask(destMask) catch unreachable;
                 try moves.append(allocator, Move.newNonPromotion(source, dest, MoveFlag.Normal));
             }
+        }
+    }
+
+    pub fn addLegalKingMoves(self: *const Position, comptime allocator: std.mem.Allocator, moves: *ArrayList(Move)) !void {
+        const source = Square.fromMask(self.board.mask(Piece.King, self.sideToMove)) catch return error.MultipleKingsForColor;
+        const attacks = singleKingAttacks(source);
+        var destMasksIter = iterSetBits(attacks);
+        while (destMasksIter.next()) |destMask| {
+            const dest = Square.fromMask(destMask) catch unreachable;
+            if (self.isKingMoveSafe(source, dest)) {
+                try moves.append(allocator, Move.newNonPromotion(source, dest, MoveFlag.Normal));
+            }
+        }
+    }
+
+    fn isKingMoveSafe(self: *const Position, source: Square, dest: Square) bool {
+        const attackers = self.colorMask(self.sideToMove.other()) & !dest.mask();
+
+        if (multiPawnAttacks(dest, self.sideToMove) & self.board.pieceMask(Piece.Pawn) & attackers != 0 or
+            singleKnightAttacks(dest) & self.board.pieceMask(Piece.Knight) & attackers != 0 or
+            singleKingAttacks(dest) & self.pieceMask(Piece.King) & attackers != 0)
+        {
+            return false;
+        } else {
+            const queens = self.board.pieceMask(Piece.Queen);
+            const relevantDiagonalAttackers = (self.board.pieceMask(Piece.Bishop) | queens) & attackers & dest.diagonalsMask();
+            const relevantOrthogonalAttackers = (self.board.pieceMask(Piece.Rook) | queens) & attackers & dest.orthogonalsMask();
+            const relevantSlidingAttackers = relevantDiagonalAttackers | relevantOrthogonalAttackers;
+
+            const occupied = self.board.occupiedMask() ^ (dest.mask() | source.mask());
+
+            var attackerSourceMasksIter = iterSetBits(relevantSlidingAttackers);
+            while (attackerSourceMasksIter.next()) |attackerSourceMask| {
+                const attackerSource = Square.fromMask(attackerSourceMask) catch unreachable;
+                const blockers = between(dest, attackerSource) & occupied;
+                if (blockers == 0) {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 };
