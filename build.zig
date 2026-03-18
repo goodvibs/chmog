c87f9f19187c3729b7e31c2f7966ee1af4a35dcf
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-03-18 13:09:20 -0700

    Add docs step to build.zig

--- build.zig
+++ build.zig
@@ -86,6 +86,14 @@ pub fn build(b: *Build) void {
     });
     b.installArtifact(lib);
 
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    const docs_step = b.step("docs", "Generate and install HTML docs into zig-out/docs");
+    docs_step.dependOn(&install_docs.step);
+
     const libUnitTests = b.addTest(.{
         .root_module = fullLibMod,
     });
