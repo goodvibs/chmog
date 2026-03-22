ae38c6029a7deed16be85ce01fbf29aa4f8d76cb
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-03-22 15:29:20 -0700

    Rework perft tests again

--- tests/perft_test.zig
+++ tests/perft_test.zig
@@ -56,68 +56,43 @@ const NodeCountLookups = struct {
     };
 };
 
-const MAX_DEPTH: usize = 32;
-
-var buf: [5 * MAX_DEPTH * @sizeOf(PositionContext)]u8 = undefined;
-var fba = std.heap.FixedBufferAllocator.init(&buf);
-var alloc = fba.allocator();
-
 fn runPerftTest(allocator: std.mem.Allocator, position: *Position, depth: u8, expected_nodes: u64) !void {
-    var pos = try Position.initial(alloc, MAX_DEPTH);
+    const nodes = try position.perft(allocator, depth);
+    try std.testing.expectEqual(expected_nodes, nodes);
+}
+
+fn runPerftTestCase(comptime fen: ?[]const u8, comptime depth: u8, comptime expected: u64) !void {
+    const numContexts = depth + 1;
+
+    var buf: [(numContexts) * @sizeOf(PositionContext)]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buf);
+    const alloc = fba.allocator();
+
+    var pos = if (fen) |f|
+        try parseFen(f, alloc, numContexts)
+    else
+        try Position.initial(alloc, numContexts);
     defer pos.contexts.deinit(alloc);
 
-    const nodes = try position.perft(allocator, depth);
-    try std.testing.expectEqual(
-        expected_nodes,
-        nodes,
-    );
+    try runPerftTest(alloc, &pos, depth, expected);
 }
 
 test "perft initial position" {
-    var pos = try Position.initial(alloc, MAX_DEPTH);
-    defer pos.contexts.deinit(alloc);
-
-    const depth = 3;
-
-    try runPerftTest(alloc, &pos, depth, NodeCountLookups.INITIAL_POSITION[depth]);
+    try runPerftTestCase(null, 3, NodeCountLookups.INITIAL_POSITION[3]);
 }
 
 test "perft kiwipete" {
-    const fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
-    var pos = try parseFen(fen, alloc, MAX_DEPTH);
-    defer pos.contexts.deinit(alloc);
-
-    const depth = 2;
-
-    try runPerftTest(alloc, &pos, depth, NodeCountLookups.KIWIPETE[depth]);
+    try runPerftTestCase("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 2, NodeCountLookups.KIWIPETE[2]);
 }
 
 test "perft position 3" {
-    const fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
-    var pos = try parseFen(fen, alloc, MAX_DEPTH);
-    defer pos.contexts.deinit(alloc);
-
-    const depth = 1;
-
-    try runPerftTest(alloc, &pos, depth, NodeCountLookups.POSITION_3[depth]);
+    try runPerftTestCase("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 1, NodeCountLookups.POSITION_3[1]);
 }
 
 test "perft position 4" {
-    const fen = "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1";
-    var pos = try parseFen(fen, alloc, MAX_DEPTH);
-    defer pos.contexts.deinit(alloc);
-
-    const depth = 3;
-
-    try runPerftTest(alloc, &pos, depth, NodeCountLookups.POSITION_4[depth]);
+    try runPerftTestCase("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 3, NodeCountLookups.POSITION_4[3]);
 }
 
 test "perft position 5" {
-    const fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
-    var pos = try parseFen(fen, alloc, MAX_DEPTH);
-    defer pos.contexts.deinit(alloc);
-
-    const depth = 2;
-
-    try runPerftTest(alloc, &pos, depth, NodeCountLookups.POSITION_5[depth]);
+    try runPerftTestCase("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 2, NodeCountLookups.POSITION_5[2]);
 }
