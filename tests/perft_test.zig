7c700fe982b820a47ac4a1ec2b1516456305714b
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-03-22 15:58:49 -0700

    Rework perft tests one more time

--- tests/perft_test.zig
+++ tests/perft_test.zig
@@ -61,38 +61,47 @@ fn runPerftTest(allocator: std.mem.Allocator, position: *Position, depth: u8, ex
     try std.testing.expectEqual(expected_nodes, nodes);
 }
 
-fn runPerftTestCase(comptime fen: ?[]const u8, comptime depth: u8, comptime expected: u64) !void {
+fn fenPositionConstructor(comptime fen: []const u8) *const fn (std.mem.Allocator, usize) anyerror!Position {
+    return struct {
+        fn call(alloc: std.mem.Allocator, cap: usize) anyerror!Position {
+            return parseFen(fen, alloc, cap);
+        }
+    }.call;
+}
+
+fn runPerftTestCase(
+    comptime createPosition: *const fn (std.mem.Allocator, usize) anyerror!Position,
+    comptime depth: u8,
+    comptime expected: u64,
+) !void {
     const numContexts = depth + 1;
 
-    var buf: [(numContexts) * @sizeOf(PositionContext)]u8 = undefined;
+    var buf: [numContexts * @sizeOf(PositionContext)]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buf);
     const alloc = fba.allocator();
 
-    var pos = if (fen) |f|
-        try parseFen(f, alloc, numContexts)
-    else
-        try Position.initial(alloc, numContexts);
+    var pos = try createPosition(alloc, numContexts);
     defer pos.contexts.deinit(alloc);
 
     try runPerftTest(alloc, &pos, depth, expected);
 }
 
 test "perft initial position" {
-    try runPerftTestCase(null, 3, NodeCountLookups.INITIAL_POSITION[3]);
+    try runPerftTestCase(Position.initial, 3, NodeCountLookups.INITIAL_POSITION[3]);
 }
 
 test "perft kiwipete" {
-    try runPerftTestCase("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 2, NodeCountLookups.KIWIPETE[2]);
+    try runPerftTestCase(fenPositionConstructor("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"), 2, NodeCountLookups.KIWIPETE[2]);
 }
 
 test "perft position 3" {
-    try runPerftTestCase("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 1, NodeCountLookups.POSITION_3[1]);
+    try runPerftTestCase(fenPositionConstructor("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"), 1, NodeCountLookups.POSITION_3[1]);
 }
 
 test "perft position 4" {
-    try runPerftTestCase("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 3, NodeCountLookups.POSITION_4[3]);
+    try runPerftTestCase(fenPositionConstructor("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1"), 3, NodeCountLookups.POSITION_4[3]);
 }
 
 test "perft position 5" {
-    try runPerftTestCase("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 2, NodeCountLookups.POSITION_5[2]);
+    try runPerftTestCase(fenPositionConstructor("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"), 2, NodeCountLookups.POSITION_5[2]);
 }
