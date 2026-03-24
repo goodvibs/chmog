eb7248b2bc306b8d9cbfaeb63dfab400d6702562
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-03-24 00:44:07 -0700

    Move MoveFlag to move.zig

--- src/move.zig
+++ src/move.zig
@@ -5,7 +5,24 @@ const Color = @import("./root.zig").Color;
 const Flank = @import("./root.zig").Flank;
 const Square = @import("./root.zig").Square;
 const PromotionPiece = @import("./root.zig").PromotionPiece;
-const MoveFlag = @import("./root.zig").MoveFlag;
+
+/// Move flag indicating the type of move.
+pub const MoveFlag = enum(u2) {
+    Normal = 0,
+    Promotion = 1,
+    EnPassant = 2,
+    Castling = 3,
+
+    /// Creates from 0-3 index.
+    pub fn fromInt(index: u2) MoveFlag {
+        return @enumFromInt(index);
+    }
+
+    /// Returns 0-3 index.
+    pub fn int(self: MoveFlag) u2 {
+        return @intFromEnum(self);
+    }
+};
 
 /// Move-related errors: ExpectedACastlingMove, ExpectedANonPromotion, BufferTooSmall.
 pub const MoveError = error{
